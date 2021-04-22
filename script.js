2f9fd0d2709d0567b7933fecd44c2f2fed90d7c4
Sah96ah <43195118+Sah96ah@users.noreply.github.com>
2021-04-23 05:10:08 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,16 @@
 function compute()
 {
     p = document.getElementById("principal").value;
+    r = document.getElementById("rate").value;
+    y = document.getElementById("years").value;
+
+    var i = p*r*y;
+    var dt = new Date();
+    var yt = dt.getFullYear()+y;
     
+    document.getElementById("pr").innerHTML = "If your deposit "+p;
+    document.getElementById("r").innerHTML = "at an interest rate of "+rate;
+    document.getElementById("result").innerHTML = "You will recieve an amount of "+i;
+    document.getElementById("yr").innerHTML = "in the year "+yt;
 }
         
