a888d098babf860131c1de39e9948e3d86591db6
Sah96ah <sah.96ah@gmail.com>
2021-04-23 07:10:19 +0530

    final commit

--- script.js
+++ script.js
@@ -4,15 +4,15 @@ function compute()
     r = document.getElementById("rate").value;
     y = document.getElementById("years").value;
 
-    var i = p*parseInt(r)*y;
+    var i = p*parseFloat(r)*y;
     var dt = new Date();
-    var yt = parseInt(dt.getFullYear())+y;
+    var yt = parseInt(dt.getFullYear())+parseInt(y);
     
     document.getElementById("pr").innerHTML = "If your deposit "+p;
-    document.getElementById("r").innerHTML = "at an interest rate of "+rate;
+    document.getElementById("r").innerHTML = "At an interest rate of "+ r +"%";
     document.getElementById("result").innerHTML = "You will recieve an amount of "+i;
-    document.getElementById("yr").innerHTML = "in the year "+yt;
+    document.getElementById("yr").innerHTML = "In the year "+yt;
 }
 function updateTextInput(val) {
-          document.getElementById('rangeInput')..innerHTML = val; 
+          document.getElementById('rangeInput').innerHTML = parseFloat(val) + "%"; 
 }        
