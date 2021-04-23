dad8101b3ad1f3fb80cdd8883330c61ce4f6096f
Sah96ah <sah.96ah@gmail.com>
2021-04-23 08:43:55 +0530

    ultimate commit

--- script.js
+++ script.js
@@ -4,14 +4,20 @@ function compute()
     r = document.getElementById("rate").value;
     y = document.getElementById("years").value;
 
-    var i = (p*parseFloat(r)*y)/100;
-    var dt = new Date();
-    var yt = parseInt(dt.getFullYear())+parseInt(y);
-    
-    document.getElementById("pr").innerHTML = "If your deposit "+p;
-    document.getElementById("r").innerHTML = "At an interest rate of "+ r +"%";
-    document.getElementById("result").innerHTML = "You will recieve an amount of "+i;
-    document.getElementById("yr").innerHTML = "In the year "+yt;
+    if (p=="" || p<=0){
+        window.alert('Enter any positive number')
+        document.getElementById("principal").focus(); 
+    }
+    else{
+        var i = (p*parseFloat(r)*y)/100;
+        var dt = new Date();
+        var yt = parseInt(dt.getFullYear())+parseInt(y);
+        
+        document.getElementById("pr").innerHTML = "If your deposit <mark>"+ p +"</mark>"
+        document.getElementById("r").innerHTML = "at an Interest rate of <mark>"+ r +"%"+"</mark>"
+        document.getElementById("result").innerHTML = "You will recieve an amount of <mark>"+ i +"</mark>"
+        document.getElementById("yr").innerHTML = "in the year <mark>"+ yt +"</mark>"
+    }
 }
 function updateTextInput(val) {
           document.getElementById('rangeInput').innerHTML = parseFloat(val) + "%"; 
